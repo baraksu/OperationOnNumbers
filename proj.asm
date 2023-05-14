b2987a1b1af582b419ddc159369b6a3eaa8259bb
PerkyChicken <eitananddaniel@gmail.com>
2023-05-14 20:24:17 +0300

    Update proj.asm
    added a few stuff

--- proj.asm
+++ proj.asm
@@ -1,9 +1,12 @@
 .MODEL small
 .STACK 100h
 .DATA        
-array db 21dup(0)
-msg db 13,10,'Enter number of numbers: ','$'        
-msg0 db 13,10,'Enter number: ','$'
+arrayOgNums db 100dup(0)
+arrayNumsValueg db 100dup(0)
+msg db 13,10,'Enter amount of numbers: ','$'        
+msg0 db 13,10,'Enter number (enter "," to finish): ','$' 
+msg1 db 13,10,'Enter next digit: ','$'     
+msg2 db 13,10,'Wrong input enter new: '
 counter db 0
 .CODE
     mov ax,@data
@@ -22,16 +25,57 @@ exit:
 proc getNumbers
     push bp 
     mov bp,sp
+
+    mov cx,counter     
+    xor bx,bx
     
-    mov ah,09h
-    lea dx,msg0
+getNumbersLoop:  
+    
+    
+getNumberLoop:
+    
+    lea dx,msg1
+    mov ax,09
     int 21h
     
-    mov ah,01h
+    mov ah,1
     int 21h
-    sub al,30h
-    mov 
+       
+    call CheckInputOnAl
+    
+    mov arrayOgNums[bx],al
+    cmp al,','
+    je getNumbersLoop
+    jmp getNumberLoop
     
     pop bp 
-endp getNumbers    
-END
+endp getNumbers     
+
+proc CheckInputOnAl
+    push bp 
+    mov bp,sp     
+    
+strt:   
+
+    cmp al,'0'
+    jb error
+    cmp al,'9'
+    ja error
+    cmp al,','
+    jne error
+    
+error:
+
+    lea dx,msg2
+    mov ah,09
+    int 21h 
+    
+    mov ah,01
+    int 21h
+    jmp strt 
+    
+finish:
+                         
+    pop bp           
+endp CheckInput    
+END  
