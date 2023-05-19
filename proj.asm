9991112eaceda2574a6787e65182d76951196705
PerkyChicken <eitananddaniel@gmail.com>
2023-05-19 14:43:22 +0300

    added start of conversion
    added start of conversion from char to true value

--- proj.asm
+++ proj.asm
@@ -3,9 +3,9 @@
 .STACK 100h
 .DATA        
 arrayOgNums db 100dup(0)
-arrayNumsValueg db 100dup(0)
+arrayNumsValue dw 100dup(0)
 msg db 13,10,'Enter amount of numbers: ','$'        
-msg0 db 13,10,'Enter number (enter "," to finish): ','$' 
+msg0 db 13,10,'Enter number (enter "," to finish)','$' 
 msg1 db 13,10,'Enter next digit: ','$'     
 msg2 db 13,10,'Wrong input enter new: ','$'
 counter db 0
@@ -31,13 +31,20 @@ exit:
 proc GetNumbers    ;gets the numbers from the user 
     pusha
     mov bp,sp
-    
      
     mov cl,counter    
     xor ch,ch
     xor bx,bx
     
     getNumbersLoop:  
+    
+    cmp cx,0
+    jbe fnsh 
+    dec cx
+    
+    mov ah,09h           
+    lea dx, msg0           
+    int 21h 
     
     getNumberLoop:
     
@@ -50,13 +57,16 @@ proc GetNumbers    ;gets the numbers from the user
        
     call CheckInputOnAl ;call a procedure that checks if the input is correct
     
-    mov arrayOgNums[bx],al
+    mov arrayOgNums[bx],al                                                 
+    inc bx     
     cmp al,','         ;checks if the user has finished inputing the number
     je getNumbersLoop  
-    jmp getNumberLoop
-         
+    jmp getNumberLoop   
+    call GetNumbersTrueValue
+     
+    fnsh:     
     popa
-   
+    ret
 endp GetNumbers     
 
 proc CheckInputOnAl           
@@ -66,11 +76,11 @@ proc CheckInputOnAl
     
     strt:   
     
-    cmp al,44h
+    cmp al,','
     je finish
-    cmp al,30h
+    cmp al,'0'
     jl error
-    cmp al,39h
+    cmp al,'9'
     jg error
     jmp finish
     
@@ -90,5 +100,49 @@ proc CheckInputOnAl
     pop bp 
     ret              
 endp CheckInputOnAl  
-  
+
+proc GetNumbersTrueValue
+    pusha
+    mov bp,sp                       
+    
+    xor si,si
+    xor cx,cx   
+    mov cl,counter            
+    xor bx,bx      
+    xor ax,ax
+    mov al,1h 
+    xor di,di
+    mov dx,10d  
+    
+    convertCharNumToTrueNumLoop:
+    mov di,bx 
+    dec di   
+      
+    getNumberOfDigits:    
+    
+    cmp arrayOgNums[bx],','
+    je convCharToNum
+    mul dx
+    inc bx
+    jmp getNumberOfDigits
+    
+    convCharToNum:   
+    inc di    
+    push ax
+    sub arrayOgNums[di],'0'       
+    mul arrayOgNums[di]
+    add arrayNumsValue[si],ax
+    pop ax
+    div dx        
+    add arrayOgNums[di],'0'
+    cmp arrayOgNums[di+1],','
+    jne convCharToNum
+     
+    inc si 
+    inc bx
+    loop convertCharNumToTrueNumLoop                            
+                              
+    popa      
+    ret    
+endp GetNumbersTrueValue  
 END  
